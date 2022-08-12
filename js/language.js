74928dd910f474de57ec6ba8c5f440458e2e9c86
Alex <pasha_pasha_201@mail.ru>
2022-08-12 22:38:29 +0300

    small changes

---
+++ js/language.js
@@ -0,0 +1,234 @@
+const langArr = {
+    "Menu__link-Home" :{
+        "en":"Home",
+        "ru":"Главная"
+    },
+    "Menu__link-About" :{
+        "en":"About",
+        "ru":"О нас"
+    },
+    "Menu__link-Product" :{
+        "en":"Product",
+        "ru":"Предложения"
+    },
+    "Menu__link-Pricing" :{
+        "en":"Pricing",
+        "ru":"Цена"
+    },
+    "preview__subtitle" :{
+        "en":"Creative Agency",
+        "ru":"Креативное агенство"
+    },
+    "preview__title" :{
+        "en":"We Deliver The Best Product for You",
+        "ru":"Мы Поставляем Лучший Продукт для Вас"
+    },
+    "preview__desc" :{
+        "en":"Design is the creation of a plan or convention for the construction of an object or a system as in architectural blueprints, engineering drawings, business processes,",
+        "ru":"Проектирование - это создание плана или соглашения для строительства объекта или системы, как в архитектурных чертежах, инженерных чертежах, бизнес-процессах,"
+    },
+    "preview__link" :{
+        "en":"Get started",
+        "ru":"Связаться с нами"
+    },
+    "services__subtitle" :{
+        "en":"Our Services",
+        "ru":"Наши Услуги"
+    },
+    "services__title" :{
+        "en":"Why People Choose Us",
+        "ru":"Почему Люди Выбирают Нас"
+    },
+    "services__block-title-design" :{
+        "en":"Design",
+        "ru":"Дизайн"
+    },
+    "services__block-desc-design":{
+        "en":"A plan or drawing produced to show the look and function",
+        "ru":"План или чертеж, созданный для демонстрации внешнего вида и функций"
+    },
+    "services__block-title-development":{
+        "en":"Development",
+        "ru":"Разработка"
+    },
+    "services__block-desc-development" :{
+        "en":"Development is defined as the process of growth or new ",
+        "ru":"Развитие определяется как процесс роста в чем нибудь новом"
+    },
+    "services__block-title-Branding": {
+        "en":"Branding",
+        "ru":"Бренды"
+    },
+    "services__block-desc-Branding" :{
+        "en":"The marketing practice of creating a name, symbol or logotype",
+        "ru":"Маркетинговая практика это создания имени, символа или логотипа"
+    },
+    "services__block-title-Illustration":{
+        "en":"Illustration",
+        "ru":"Иллюстрация"
+    },
+    "services__block-desc-Illustration":{
+        "en":"An illustration is a decoration, interpretation or visualn",
+        "ru":"Иллюстрация - это украшение, интерпретация или визуальный элемент."
+    },
+    "services__block-link1":{
+        "en":"Learn more",
+        "ru":"Узнать больше"
+    },
+    "services__block-link2":{
+        "en":"Learn more",
+        "ru":"Узнать больше"
+    },
+    "services__block-link3":{
+        "en":"Learn more",
+        "ru":"Узнать больше"
+    },
+    "services__block-link4":{
+        "en":"Learn more",
+        "ru":"Узнать больше"
+    },
+    "things__block-subtitle" :{
+        "en":"Design Things",
+        "ru":"Дизайнерские технологии"
+    },
+    "things__block-title" :{
+        "en":"We Loves To Design Your Creative Ideas",
+        "ru":"Мы Любим Проектировать Ваши Творческие Идеи"
+    },
+    "things__block-desc" :{
+        "en":"A creative concept is an overarching “Big Idea” that captures audience interest, influences their emotional response and inspires them to take action.It is a unifying theme that can be used across all campaign messages. ",
+        "ru":"Креативная концепция - это всеобъемлющая “Большая идея”, которая привлекает интерес аудитории, влияет на их эмоциональную реакцию и вдохновляет их на действия.Это объединяющая тема, которую можно использовать во всех сообщениях кампании."
+    },
+    "things__block-link" :{
+        "en":"Learn more",
+        "ru":"Узнать больше"
+    },
+    "TeamWork__block-subtitle" :{
+        "en":"Our Teamwork",
+        "ru":"Наша Команда"
+    },
+    "TeamWork__block-title" :{
+        "en":"We Believe Success Lies On Teamwork",
+        "ru":"Успех Зависит От Командной Работы"
+    },
+    "TeamWork__block-desc" :{
+        "en":"Good teamwork means a synergistic way of working with each person committed and working towards a shared goal. ... It is therefore a necessity that leaders facilitate and build the teamwork skills of their people.",
+        "ru":"Хорошая командная работа означает синергетический подход к работе, когда каждый человек предан делу и работает над достижением общей цели. ... Поэтому необходимо, чтобы лидеры поощряли и развивали навыки командной работы у своих сотрудников."
+    },
+    "TeamWork__block-link" :{
+        "en":"Learn more",
+        "ru":"Узнать больше"
+    },
+    "Testimonial__subtitle" :{
+        "en":"Testimonial",
+        "ru":"Рекомендации"
+    },
+    "Testimonial__title" :{
+        "en":"Thats What Our Client Says About Us",
+        "ru":"Что о нас говорят клиенты"
+    },
+    "Testimonial__slider-comment1" :{
+        "en":"“ Heartiest congratulations to the team for Future projects. I extend thankful wishes and wish the team best of luck for the future assignments to come. Your effort is gladly ”",
+        "ru":"“ Сердечно поздравляю команду с будущими проектами. Я выражаю слова благодарности и желаю команде удачи в будущих проектах. Ваши работы идеальны. ”"
+    },
+    "Testimonial__slider-comment2" :{
+        "en":"“ Very cool company! We use a rented server, everything is stable! The support is super, lightning fast, thanks a lot to the guys for such service and the highest culture of customer service! Keep it up ! ”",
+        "ru":"“Очень крутая компания! Мы используем арендованный сервер, все стабильно! Поддержка супер, молниеносная, большое спасибо ребятам за такой сервис и высочайшую культуру обслуживания клиентов! Продолжай в том же духе! ”"
+    },
+    "Testimonial__slider-comment3" :{
+        "en":"“ Brand new data center. The work is connected with such places. They have an unpleasant SCUD, of course. In each passed door, you need to lean your card and not forget. ”",
+        "ru":"“ Совершенно новый центр обработки данных. Работа связана с такими местами. Конечно, у них неприятный СКАД. В каждой пройденной двери нужно прислонять свою карточку и не забывать.”"
+    },
+    "subscribe__title" :{
+        "en":"Subscribe To Our Blog",
+        "ru":"Подпишитесь На Наш Блог"
+    },
+    "subscribe__desc" :{
+        "en":"A newsletter is a tool used to communicate regularly with your subscribers, delivering the information",
+        "ru":"Рассылка новостей - это инструмент, используемый для регулярного общения с вашими подписчиками, предоставления информации"
+    },
+    "subscribe__title" :{
+        "en":"Subscribe To Our Blog",
+        "ru":"Подпишитесь На Наш Блог"
+    },
+    "subinp":{
+        "en":"Type Your Email Address",
+        "ru":"Введите вашу почту"  
+    },
+    "subbtn":{
+        "en":"Send Now",
+        "ru":"Подписаться"  
+    },
+    "footer__adress":{
+        "en":"560 N Riverview D Ste 658 <br/> North Carolina DA 598745 <br/> USA",
+        "ru":"560 N Riverview D Ste 658 <br/> Северная Каролина DA 598745 <br/> США"  
+    },
+    "footer__follow":{
+        "en":"Follow us on",
+        "ru":"Подпишитесь на нас"  
+    },
+    "footer__block-title-Resouces": {
+        "en":"Resouces",
+        "ru":"Ресурсы" 
+    },
+    "footer__link-Revisions":{
+        "en":"Revisions",
+        "ru":"Переделки"  
+    },
+    "footer__link-Sheets":{
+        "en":"Sheets",
+        "ru":"Таблица"  
+    },
+    "footer__link-Quiz":{
+        "en":"Quiz",
+        "ru":"Викторина"  
+    },
+    "footer__link-Drill":{
+        "en":"Drill",
+        "ru":"Упражнение"  
+    },
+    "footer__block-title-About": {
+        "en":"About",
+        "ru":"Информация" 
+    },
+    "footer__link-About":{
+        "en":"About us",
+        "ru":"О нас" 
+    },
+    "footer__link-Partners":{
+        "en":"Partners",
+        "ru":"Партнеры"  
+    },
+    "footer__link-Contact":{
+        "en":"Contact us",
+        "ru":"Контакты"  
+    },
+    "footer__link-Careers":{
+        "en":"Careers",
+        "ru":"Карьера"  
+    },
+    "footer__block-title-Helpful": {
+        "en":"Helpful link",
+        "ru":"Полезные ссылки" 
+    },
+    "footer__link-Mirage":{
+        "en":"Mirage",
+        "ru":"Мираж" 
+    },
+    "footer__link-Solutions":{
+        "en":"Solutions",
+        "ru":"Решения"  
+    },
+    "footer__link-Industries":{
+        "en":"Industries",
+        "ru":"Индустрия"  
+    },
+    "footer__link-Services":{
+        "en":"Services",
+        "ru":"Услуги"  
+    },
+    "copyright__inner-copy":{
+        "en":"© All Rights Reserved By Mirage 2020",
+        "ru":"© Все Права Защищены Mirage 2020"  
+    }
+}
--- js/main.js
+++ js/main.js
@@ -13,5 +13,35 @@ $(document).ready(function(){
         $('.header__burger, .menu').toggleClass('active');
         $('body').toggleClass('lock');
     });
+
+    const selectLanguage = document.querySelector('.change-lang');
+const allLang = ['en', 'ru'];
+selectLanguage.addEventListener('change', changeURLLanguage);
+
+function changeURLLanguage (){
+    let lang = selectLanguage.value;
+    location.href = window.location.pathname + '#' + lang;
+    location.reload();
+}
+
+function ChangeLanguage(){
+    let hash = window.location.hash;
+    hash = hash.substr(1);
+    console.log(hash);
+    if (!allLang.includes(hash)){
+        location.href = window.location.pathname + '#en';
+        location.reload();
+    }
+    selectLanguage.value  =  hash;
+    for (let key in langArr){
+        document.querySelector('.'+key).innerHTML=langArr[key][hash];
+    }
+    document.querySelector('.preview__link').innerHTML=langArr['preview__link'][hash]+'<img src="./img/arrow.svg" class="preview__link-img">';
+    document.querySelector('.things__block-link').innerHTML=langArr['things__block-link'][hash]+'<img src="./img/arrow.svg" class="preview__link-img">';
+    document.querySelector('.TeamWork__block-link').innerHTML=langArr['TeamWork__block-link'][hash]+'<img src="./img/arrow.svg" class="preview__link-img">';
+    document.querySelector('.subinp').placeholder=langArr['subinp'][hash];
+    document.querySelector('.subbtn').value=langArr['subbtn'][hash];
+}
+ChangeLanguage();
   });
           
